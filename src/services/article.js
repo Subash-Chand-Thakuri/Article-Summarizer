e59823a3d9dd70908fc1a4937774d4a3147c6fba
Subash <subashthakuri799@gmail.com>
2024-02-13 19:25:53 +0545

    Ai article summarizer

---
+++ src/services/article.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+
+const rapidapiKey = import.meta.envVITE_RAPID_API_ARTICLE_KEY
+  
+
+export const articleApi = createApi({
+    reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl:'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key',rapidapiKey),
+            headers.set('X-RapidAPI-Host','article-extractor-and-summarizer.p.rapidapi.com');
+
+            return headers;
+        }
+    }),
+    endpoints:(builder) => ({
+        getSummary : builder.query({
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
+        })
+    })
+})
+
+export const { useLazyGetSummaryQuery } = articleApi;
